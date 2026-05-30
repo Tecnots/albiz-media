b2584bc7500b77e84b067166364e0d5ac3e11b2a
akhineshkv-tecnots <akhinesh.kv@Tecnots.com>
2026-05-30 11:39:01 +0530

    author module phase 1

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -211,6 +211,6 @@ const authOptions = {
   },
 };
 
-const handler = NextAuth(authOptions);
+const { handlers } = NextAuth(authOptions);
 
-export { handler as GET, handler as POST };
+export const { GET, POST } = handlers;
